64d9b7520e1ea56a3c5fac9282905b93fe558b70
Bryan Burciaga <bryanburciaga@Bryans-MacBook-Pro.local>
2022-10-18 11:09:00 -0600

    detail.html working

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home),
+    path('expenses/<int:expense_id>/', views.expense_detail, name='expenses_detail'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from .models import Expense
 
 
 def home(request):
-    return render(request, 'home.html')
+    expenses = Expense.objects.filter()
+    return render(request, 'home.html', {'expenses': expenses})
+
+def expense_detail(request, expense_id):
+    expense = Expense.objects.get(id=expense_id)
+    return render(request, 'expenses/detail.html', {
+        'expense': expense,
+    })
 
