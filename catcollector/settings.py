1fc83fa0c729c4a2281f4f537282b93603f25141
Bryan Burciaga <bryanburciaga@Bryans-MacBook-Pro.local>
2022-10-19 16:40:43 -0600

    prepares for deployment

--- catcollector/settings.py
+++ catcollector/settings.py
@@ -12,18 +12,19 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+import environ
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
+env = environ.Env()
+environ.Env.read_env()
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-np90*3wn-32$bz%45tkxy8irvqdf7i63c7he3v_w77)t%&zvyt'
-
+SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG') == 'True'
 
 ALLOWED_HOSTS = []
 
@@ -131,3 +132,9 @@ LOGOUT_REDIRECT_URL = '/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import django_heroku 
+django_heroku.settings(locals())
+
+
+DEBUG_PROPAGATE_EXCEPTIONS = True
